7af507bb99a995c3b0a0a5f231f9a56a105173c2
Jason William Ellsworth <jasonells48@icloud.com>
2022-07-26 22:48:09 -0400

    updated some stuff with javascript

---
+++ fun3.js
@@ -0,0 +1,17 @@
+//add 7, takes a number and adds 7!
+
+function number(num) {
+    let value = num + 7;
+    return value;
+}
+
+// take two values, than multiply
+
+
+// converts letters to upper case
+
+function capital(word) {
+   let capitalized = word[0].toUpperCase();
+   return capitalized;
+   
+}
